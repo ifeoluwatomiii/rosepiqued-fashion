e837e3c3f5949064b2d809758df6c792d68790d9
ifemi27 <opatolaifeoluwav@gmail.com>
2023-04-20 15:08:05 -0700

    image added to the furniture page

--- src/pages/Furnitures.jsx
+++ src/pages/Furnitures.jsx
@@ -1,10 +1,15 @@
 import React, {useState} from 'react'
+import chair from "../assets/chair.jpg"
 
 
 const Furnitures = () => {
 	
   return (
-	<div>Furnitures</div>
+	<div>
+    <div>
+      <img src={chair} alt="" />
+    </div>
+  </div>
   )
 }
 
